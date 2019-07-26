da1a5a49ec922a6ae9ec3eb78ef53b5473f9e8e1
Guillaume Macret <guillaume.macret@worldline.com>
2019-07-26 17:13:43 +0200

    :tada: First commit

---
+++ Assets/Scripts/AbstractFightingEntity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractFightingEntity : MonoBehaviour
+{
+    public const int MAX_SPEED = 255;
+
+    private static int debugId = 0;
+
+    /*## private var ##*/
+    int id;
+    //Speed at start of the fight (stat + items)
+    int baseSpeed;
+    //Actual speed compting buff and debuff applied while fighting
+    int actualSpeed;
+
+    /*## public var ##*/
+    private int cptSpeed;
+    public int CptSpeed { get => cptSpeed; set => cptSpeed = value; }
+    private bool playerControlled;
+    public bool IsPlayerControlled { get => playerControlled; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public AbstractFightingEntity(int cptSpeed)
+    {
+        this.cptSpeed = cptSpeed;
+        this.id = debugId++;
+        playerControlled = true;
+    }
+
+    public override String ToString()
+    {
+        return ("Abstract Fighting Entity: {id : " + id + ", cptSpeed : " + cptSpeed + "}");
+    }
+
+    public void IncreaseCptSpeed(int amount)
+    {
+        CptSpeed += amount;
+    }
+
+    public bool CanPlay()
+    {
+        return CptSpeed > MAX_SPEED;
+    }
+
+    public void Play()
+    {
+        throw new NotImplementedException();
+        bool hasPlayed = false;
+
+        if (hasPlayed)
+        {
+            ResetCptSpeed();
+        }
+    }
+
+    private void ResetCptSpeed()
+    {
+        CptSpeed = actualSpeed;
+    }
+}
---
+++ Assets/Scripts/CombatEngine.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatEngine : MonoBehaviour
+{
+    public List<AbstractFightingEntity> fightingEntities;
+    
+    private bool lockTimer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AbstractFightingEntity e1 = new AbstractFightingEntity(30);
+        AbstractFightingEntity e2 = new AbstractFightingEntity(10);
+        AbstractFightingEntity e3 = new AbstractFightingEntity(40);
+        AbstractFightingEntity e4 = new AbstractFightingEntity(50);
+        fightingEntities.Add(e1);
+        fightingEntities.Add(e2);
+        fightingEntities.Add(e3);
+        fightingEntities.Add(e4);
+        GetMaxEntitiesCptSpeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!lockTimer)
+        {
+            IncreaseEntitiesCptSpeed(AbstractFightingEntity.MAX_SPEED - GetMaxEntitiesCptSpeed());
+        }
+        else
+        {
+            Debug.Log("Blocked");
+            Debug.Log(fightingEntities);
+        }//TODO TEST
+    }
+
+    private int GetMaxEntitiesCptSpeed()
+    {
+        int maxSpeedFound = int.MinValue;
+        //AbstractFightingEntity nextEntiy = null;
+        foreach (AbstractFightingEntity entity in fightingEntities)
+        {
+            if(entity.CptSpeed > maxSpeedFound)
+            {
+                maxSpeedFound = entity.CptSpeed;
+                //nextEntiy = entity;
+            }
+        }
+        return maxSpeedFound;
+    }
+
+    private void IncreaseEntitiesCptSpeed(int amount)
+    {
+        foreach (AbstractFightingEntity entity in fightingEntities)
+        {
+            entity.IncreaseCptSpeed(amount);
+            if (entity.CanPlay() && entity.IsPlayerControlled)
+            {
+                lockTimer = true;
+            }
+        }
+    }
+
+    public void MakeEntitiesPlay()
+    {
+        foreach(AbstractFightingEntity entity in fightingEntities)
+        {
+            if (entity.CanPlay())
+            {
+                entity.Play();
+            }
+        }
+    }
+}
