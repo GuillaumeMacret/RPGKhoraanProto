97ed3b3bcca73da22091554e6d6b7c2aeca7e75d
Guillaume Macret <guillaume.macret@gmail.com>
2019-08-30 12:02:40 +0200

    :bug: Fixing health number at fight init

--- Assets/Scripts/Combat/CombatStatistics.cs
+++ Assets/Scripts/Combat/CombatStatistics.cs
@@ -10,7 +10,7 @@ public class CombatStatistics : MonoBehaviour
     public int speed;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         currentHp = maxHp;
     }
